1edfebdaad0e2ed87e37ebed309aadbbccaa340b
BorisBanchev <boris.banchev@helsinki.com>
2024-11-03 19:08:43 +0200

    viikko2 teht.2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,16 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
+        
+        parsed_toml = toml.loads(content)
+        information = parsed_toml["tool"]["poetry"]
+        name = information["name"]
+        description = information["description"]
+        license = information["license"]
+        authors = information["authors"]
+        dependencies = list(information["dependencies"])
+        devdependencies = list(information["group"]["dev"]["dependencies"])
+                               
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        
+        return Project(name, description, license, authors, dependencies, devdependencies)
